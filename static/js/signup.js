0b3877f6c906b5f84f51e8a5be91043e97ceb366
Bornfaceobure646 <114803686+Bornfaceobure646@users.noreply.github.com>
2024-02-27 14:14:18 +0300

    Log form data to console. (generated by blackbox.ai )

--- static/js/signup.js
+++ static/js/signup.js
@@ -1,6 +1,7 @@
 const form  = document.querySelector('form')
 form.addEventListener('submit',(e)=> { e.preventDefault();
   
-  const fd = new FormData(form)
-  const Obj = Object.fromEntries(fd)
+  const formData = new FormData(form)
+  for (item of formData)
+  console.log(item[0],item[1]);
 })
