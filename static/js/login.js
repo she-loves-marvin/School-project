62890610abf7c8f2e4f7bfaf21182241d6a693fd
Bornfaceobure646 <114803686+Bornfaceobure646@users.noreply.github.com>
2024-03-01 17:07:56 +0300

    update

--- static/js/login.js
+++ static/js/login.js
@@ -1,7 +1,15 @@
-const form = document.querySelector('form')
-form.addEventListener('sign In', function(){
-
-    const formData= new FormData();
-    const email= document.getElementById("email");
-    const Password =document.getElementById("[password");
-})
+document.addEventListener('DOMContentLoaded', function () {
+    const submitButton = document.querySelector('input[type="button"]');
+    submitButton.addEventListener('click', processData);
+  
+  });
+  function processData() {
+    const email= document.getElementById("email").value;
+    const password=document.getElementById("password").value;
+    
+    if (validateEmail(email) && validatePassword(password)) {
+      alert("Logged in successfully");}
+    else{
+      showError();
+    }
+}
--- static/js/signup.js
+++ static/js/signup.js
@@ -1,14 +1,15 @@
-const form = document.querySelector('form')
+document.addEventListener('DOMContentLoaded', function () {
+  const submitButton = document.querySelector('input[type="button"]');
+  submitButton.addEventListener('click', processData);
 
-form.addEventListener('submit',function() {
-  const formData= new FormData();
-
-  const email = document.getElementById("email");
-
-  const Password = document.getElementById("Password");
-
-
-FormData.push(FormData);
-
-document.querySelector('form').reset()
-})
+});
+function processData() {
+  const email= document.getElementById("email").value;
+  const password=document.getElementById("password").value;
+  
+  if (validateEmail(email) && validatePassword(password)) {
+    alert("Logged in successfully");}
+  else{
+    showError();
+  }
+}
